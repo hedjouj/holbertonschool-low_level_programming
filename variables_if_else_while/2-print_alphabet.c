5101afbc126e4da63db4c95fa4ebf034a54f430f
hedjouj <messaoui.elhadj@hotmail.fr>
2025-02-19 09:25:12 +0000

    2. I sometimes suffer from insomnia

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
-/*
- *
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
  */
 
 int main(void)
@@ -8,7 +9,7 @@ int main(void)
 	char ch;
 
 	for (ch = 'a'; ch <= 'z'; ch++)
-	putchar(ch);
+		putchar(ch);
 	putchar('\n');
 	return (0);
 }
