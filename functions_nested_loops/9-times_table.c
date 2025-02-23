156f26769f64c9b43c1e36d478365448705f0f9e
hedjouj <messaoui.elhadj@hotmail.fr>
2025-02-23 17:50:06 +0000

    9. Learn your times table

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -14,23 +14,26 @@ void times_table(void)
 		{
 			product = row * col;
 
-			if (col != 0)
+			if (col == 0)
 			{
+				_putchar(product + '0');
+			}
+			else
+			{
+				_putchar(' ');
 				_putchar(',');
-				_putchar(' ');
-			
-			if (product < 10)
-			{
-				_putchar(' ');
+				if (product < 10)
+				{
+					_putchar(' ');
+				}
+				if (product >= 10)
+				{
+					_putchar((product / 10) + '0');
+				}
+				_putchar((product % 10) + '0');
 			}
-			if (product >= 10)
-			{
-				_putchar((product / 10) + '0');
-			}
-			_putchar((product % 10) + '0');
 		}
 
 		_putchar('\n');
-	}
 	}
 }
