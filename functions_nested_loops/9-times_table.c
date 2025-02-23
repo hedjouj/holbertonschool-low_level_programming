596329e30654ee679c5da9644db0dbc5c0abd508
hedjouj <messaoui.elhadj@hotmail.fr>
2025-02-23 17:43:16 +0000

    Write a function that prints the 9 times table, starting with 0.

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -18,10 +18,11 @@ void times_table(void)
 			{
 				_putchar(',');
 				_putchar(' ');
-			}
+			
 			if (product < 10)
+			{
 				_putchar(' ');
-
+			}
 			if (product >= 10)
 			{
 				_putchar((product / 10) + '0');
@@ -30,5 +31,6 @@ void times_table(void)
 		}
 
 		_putchar('\n');
+	}
 	}
 }
