eb992ef2db5bcab877e74959cb119e5c36011845
hedjouj <messaoui.elhadj@hotmail.fr>
2025-02-24 10:57:29 +0000

    0. isupper

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * int _isupper check if its an uppercase.
+ * @c: letter
+ * Return: Always 0 or 1
+ */
+int _isupper(int c)
+{
+
+	c = 'A';
+
+	if (c == 'A')
+	{
+		return ('1');
+	}
+	c = 'a';
+
+	if (c == 'a')
+	{
+		return ('0');
+	}
+	return (0);
+}
