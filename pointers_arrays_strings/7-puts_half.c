e78c3283cedc853c7d95975a10b53c54c4772a73
hedjouj <messaoui.elhadj@hotmail.fr>
2025-02-28 09:34:10 +0000

    prints half of a string, followed by a new line.

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -18,13 +18,12 @@ void puts_half(char *str)
 	if (j % 2 == 1)
 	{
 		k = (j + 1) / 2;
-		k += 1;
 	}
 	else
 	{
 		k = j / 2;
 	}
-	for (; k < j; k++)
+	for (; str[k] < j; k++)
 	{
 		_putchar(str[k]);
 	}
