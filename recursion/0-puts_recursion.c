9f1d928572da27f3fd7bc9af17b3333205d8a8cf
hedjouj <messaoui.elhadj@hotmail.fr>
2025-03-05 09:47:11 +0000

    Write a function that prints a string, followed by a new line and a readme

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string
+ * @s: the pointer
+ * Return: Always 0.
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+}
