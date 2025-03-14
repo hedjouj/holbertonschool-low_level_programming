a707032ae4a14e0c1c928daa381012a5bfac076b
hedjouj <messaoui.elhadj@hotmail.fr>
2025-03-14 12:34:27 +0000

    Write a function that creates a new dog.

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -15,13 +15,12 @@
  *   age, and owner.
  *   It allocates memory for the new structure and copies the name
  *   and owner strings.
- *   Returns a pointer to the newly created dog structure if successful,
+ * Returns a pointer to the newly created dog structure if successful,
  *   or NULL if memory allocation fails.
  *
  * Return:
  *   Pointer to the newly created dog structure or NULL on failure.
  */
-
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *nouveau_chien;
@@ -32,6 +31,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	nouveau_chien = malloc(sizeof(dog_t));
 	if (nouveau_chien == NULL)
 	{
+		free(nouveau_chien);
 		return (NULL);
 	}
 	while (name[longueur_name] != '\0')
@@ -39,17 +39,23 @@ dog_t *new_dog(char *name, float age, char *owner)
 	(*nouveau_chien).name = malloc(longueur_name * sizeof(char) + 1);
 	if ((*nouveau_chien).name == NULL)
 	{
+		free((*nouveau_chien).name);
+		free(nouveau_chien);
 		return (NULL);
 	}
 	while (owner[longueur_owner] != '\0')
 		longueur_owner += 1;
 	(*nouveau_chien).owner = malloc(longueur_owner * sizeof(char) + 1);
+	if ((*nouveau_chien).owner == NULL)
 	{
+		free((*nouveau_chien).owner);
+		free((*nouveau_chien).name);
+		free(nouveau_chien);
 		return (NULL);
 	}
-	for (i = 0; i <= longueur_name; i++)
+	for (i = 0 ; i <= longueur_name ; i++)
 		(*nouveau_chien).name[i] = name[i];
-	for (j = 0; j <= longueur_owner; j++)
+	for (j = 0 ; j <= longueur_owner ; j++)
 		(*nouveau_chien).owner[j] = owner[j];
 	(*nouveau_chien).age = age;
 	return (nouveau_chien);
