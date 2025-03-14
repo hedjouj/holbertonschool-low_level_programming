27dc63d51766feddbd4bf85f0407c809b4253259
hedjouj <messaoui.elhadj@hotmail.fr>
2025-03-14 12:00:43 +0000

    Write a function that creates a new dog.

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -35,7 +35,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 	while (name[longueur_name] != '\0')
-		longueur_name +=1;
+		longueur_name += 1;
 	(*nouveau_chien).name = malloc(longueur_name * sizeof(char) + 1);
 	if ((*nouveau_chien).name == NULL)
 	{
@@ -43,13 +43,13 @@ dog_t *new_dog(char *name, float age, char *owner)
 	}
 	while (owner[longueur_owner] != '\0')
 		longueur_owner += 1;
-	(*nouveau_chien).owner = malloc(longueur_owner * sizeof(char) +1);
+	(*nouveau_chien).owner = malloc(longueur_owner * sizeof(char) + 1);
 	{
 		return (NULL);
 	}
 	for (i = 0; i <= longueur_name; i++)
 		(*nouveau_chien).name[i] = name[i];
-	for (j = 0; j<= longueur_owner; j++)
+	for (j = 0; j <= longueur_owner; j++)
 		(*nouveau_chien).owner[j] = owner[j];
 	(*nouveau_chien).age = age;
 	return (nouveau_chien);
