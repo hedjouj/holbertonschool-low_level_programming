40975058ff467e60c14d1b43514590b82e822fb5
hedjouj <messaoui.elhadj@hotmail.fr>
2025-03-19 12:58:00 +0000

    corrected all betty

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -1,61 +1,60 @@
 #include "3-calc.h"
 
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
-
 /**
- * op_add - additionne deux nombres
+ * op_add - computes the sum of two numbers
  * @a: first number
  * @b: second number
+ *
+ * Return: result
  */
-
 int op_add(int a, int b)
 {
 	return (a + b);
 }
 
 /**
- * returns the difference of a and b
- * * @a: first number
+ * op_sub - computes the difference of two numbers
+ * @a: first number
  * @b: second number
+ *
+ * Return: result
  */
-
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
 
 /**
- * op_mul - returns the product of a and b
- * * @a: first number
+ * op_mul - computes the product of two numbers
+ * @a: first number
  * @b: second number
+ *
+ * Return: result
  */
-
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
 
 /**
- * op_div: returns the result of the division of a by b
- * * @a: first number
+ * op_div - computes the ...
+ * @a: first number
  * @b: second number
+ *
+ * Return: result
  */
-
 int op_div(int a, int b)
 {
 	return (a / b);
 }
 
 /**
- * op_mod: returns the remainder of the division of a by b
- * * @a: first number
+ * op_mod - computes the ...
+ * @a: first number
  * @b: second number
+ *
+ * Return: result
  */
-
 int op_mod(int a, int b)
 {
 	return (a % b);
