e16dfa8a86dfe1082e69a3fc14c34aa499a7d21f
hedjouj <messaoui.elhadj@hotmail.fr>
2025-04-01 09:13:39 +0000

    task2

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+/**
+ * add_node - ajoute un noeud au dÃbut
+ * @h: A pointer to the head of the linked list
+ *
+ * Return: the new node
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *nouveau_noeud;
+	int count = 0, i;
+	char *duplique;
+
+	nouveau_noeud = malloc(sizeof(list_t));
+	if (nouveau_noeud == NULL)
+	{
+		return (NULL);
+	}
+	duplique = strdup(str);
+	if (duplique == NULL)
+	{
+		free(nouveau_noeud);
+		return (NULL);
+	}
+	for (i = 0 ; str[i] != '\0' ; i++)
+	{
+		count++;
+	}
+	nouveau_noeud->str = duplique;
+	nouveau_noeud->len = count;
+	nouveau_noeud->next = *head;
+	*head = nouveau_noeud;
+	return (nouveau_noeud);
+}
