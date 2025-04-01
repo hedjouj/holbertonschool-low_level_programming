571fb9af1800c40c30695b51345ca71414cbe99d
hedjouj <messaoui.elhadj@hotmail.fr>
2025-04-01 09:32:34 +0000

    task3

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * add_node - Adds a new node at the beginning of a linked list
+ * add_node_end - Adds a new node at the end of a linked list
  * @head: A pointer to a pointer to the head of the linked list
  * @str: The string to be added to the new node
  *
